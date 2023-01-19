1bfce697d4ca6d482cb8545c814a75e55b237f11
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-01-19 22:09:32 +0900

    refector: apply route in App.js
    1. App.js에 라우팅 적용
    2. Home.js 라우팅 적용
    3. Detail.js 라우팅 적용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import Detail from './routes/Detail';
+
 function App() {
     return (
-        <div>
-            <h1>Welcome back!</h1>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/movie/:id' element={<Detail />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
